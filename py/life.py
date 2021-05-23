333857835c423543a451f6ecec02a0055dfc3c73
unknown <unknown@example.com>
2021-05-24 01:44:07 +0900

    life.py

--- py/life.py
+++ py/life.py
@@ -1,99 +1,111 @@
 import pygame
-import numpy as np
-
-w = 800
-h = 600
-cell = 10
-row = int(h / cell)
-col = int(w / cell)
-
-pygame.init()
-pygame.display.set_caption("Life Game")
-screen = pygame.display.set_mode((w, h))
-clock = pygame.time.Clock()
-
-white = (255, 255, 255)
-black = (0, 0, 0)
-blue = (50, 153, 213)
+import random
 
 
-def generation(arr):
-    tmp = np.zeros((row + 2, col + 2), dtype=int)
-    # 行
-    for y in range(1, row + 1):
-        # 列
-        for x in range(1, col + 1):
-            # 8方向のうち、生きているセルをカウント
-            count = 0
-            # 上
-            count += arr[y - 1][x]
-            # 右上
-            count += arr[y - 1][x + 1]
-            # 右
-            count += arr[y][x + 1]
-            # 右下
-            count += arr[y + 1][x + 1]
-            # 下
-            count += arr[y + 1][x]
-            # 左下
-            count += arr[y + 1][x - 1]
-            # 左
-            count += arr[y][x - 1]
-            # 左上
-            count += arr[y - 1][x - 1]
+class Life:
+    def __init__(self, width=800, height=600):
+        self.width = width
+        self.height = height
+        self.cell = 10
+        self.col = int(self.width / self.cell)
+        self.row = int(self.height / self.cell)
+        self.board = self.make_board()
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+        self.blue = (50, 153, 213)
 
-            # 誕生
-            # 死んでいるセルに隣接する生きたセルがちょうど3つあれば、次の世代が誕生する。
-            if arr[y][x] == 0 and count == 3:
-                tmp[y][x] = 1
+    def make_board(self):
+        board = []
+        for _ in range(self.row):
+            cols = []
+            for _ in range(self.col):
+                cols.append(random.randint(0, 1))
+            board.append(cols)
 
-            # 生存
-            # 生きているセルに隣接する生きたセルが2つか3つならば、次の世代でも生存する。
-            elif arr[y][x] == 1 and (count == 2 or count == 3):
-                tmp[y][x] = 1
+        return board
 
-            # 過疎
-            # 生きているセルに隣接する生きたセルが1つ以下ならば、過疎により死滅する。
-            elif arr[y][x] == 1 and count <= 1:
-                tmp[y][x] = 0
+    def draw(self, pygame, screen):
+        # 行
+        for row in range(self.row):
+            # 列
+            for col in range(self.col):
+                r = row * self.cell
+                c = col * self.cell
+                if self.board[row][col] == 1:
+                    pygame.draw.rect(screen, self.blue,
+                                     (c, r, self.cell, self.cell))
+                else:
+                    pygame.draw.rect(screen, self.white,
+                                     (c, r, self.cell, self.cell), 1)
 
-            # 過密
-            # 生きているセルに隣接する生きたセルが4つ以上ならば、過密により死滅する。
-            elif arr[y][x] == 1 and count >= 4:
-                tmp[y][x] = 0
+    def generation(self):
+        tmp = [[0 for _ in range(self.col)] for _ in range(self.row)]
+        # 行
+        for row in range(self.row):
+            # 列
+            for col in range(self.col):
+                # 8方向のうち、生きているセルをカウント
+                count = 0
+                if row - 1 >= 0:
+                    # 上
+                    count += self.board[row - 1][col]
+                if row - 1 >= 0 and col + 1 < self.col:
+                    # 右上
+                    count += self.board[row - 1][col + 1]
+                if col + 1 < self.col:
+                    # 右
+                    count += self.board[row][col + 1]
+                if row + 1 < self.row and col + 1 < self.col:
+                    # 右下
+                    count += self.board[row + 1][col + 1]
+                if row + 1 < self.row:
+                    # 下
+                    count += self.board[row + 1][col]
+                if row + 1 < self.row and col - 1 >= 0:
+                    # 左下
+                    count += self.board[row + 1][col - 1]
+                if col - 1 >= 0:
+                    # 左
+                    count += self.board[row][col - 1]
+                if row - 1 >= 0 and col - 1 >= 0:
+                    # 左上
+                    count += self.board[row - 1][col - 1]
 
-    return tmp
+                # 誕生
+                # 死んでいるセルに隣接する生きたセルがちょうど3つあれば、次の世代が誕生する。
+                if self.board[row][col] == 0 and count == 3:
+                    tmp[row][col] = 1
 
+                # 生存
+                # 生きているセルに隣接する生きたセルが2つか3つならば、次の世代でも生存する。
+                elif self.board[row][col] == 1 and (count == 2 or count == 3):
+                    tmp[row][col] = 1
 
-def draw(arr):
-    # 行
-    for y in range(1, row + 1):
-        # 列
-        for x in range(1, col + 1):
-            r = x * cell - cell
-            c = y * cell - cell
-            if arr[y][x] == 1:
-                pygame.draw.rect(screen, blue, (r, c, cell, cell))
-            else:
-                pygame.draw.rect(screen, white, (r, c, cell, cell), 1)
+                # 過疎
+                # 生きているセルに隣接する生きたセルが1つ以下ならば、過疎により死滅する。
+                elif self.board[row][col] == 1 and count <= 1:
+                    tmp[row][col] = 0
 
+                # 過密
+                # 生きているセルに隣接する生きたセルが4つ以上ならば、過密により死滅する。
+                elif self.board[row][col] == 1 and count >= 4:
+                    tmp[row][col] = 0
 
-def start():
-    arr = np.zeros((row + 2, col + 2), dtype=int)
-    randarr = np.random.randint(0, 2, (row, col))
-    arr[1:-1, 1:-1] = randarr
-
-    return arr
+        self.board = tmp
 
 
 def main():
-    arr = start()
+    pygame.init()
+    pygame.display.set_caption("Life Game")
+    obj = Life()
+    screen = pygame.display.set_mode((obj.width, obj.height))
+    clock = pygame.time.Clock()
 
     while True:
         clock.tick(10)
-        screen.fill(white)
-        arr = generation(arr)
-        draw(arr)
+        screen.fill(obj.white)
+        obj.generation()
+        obj.draw(pygame, screen)
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -107,7 +119,7 @@ def main():
                     quit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                arr = start()
+                main()
 
 
 if __name__ == "__main__":
